bbce3233cdf147b411bce8e9d17c72c5a54824f0
Peyman <hassani.peyman627@gmail.com>
2024-08-27 13:46:36 +0330

    Setup channels in asgi.py

--- omcb/asgi.py
+++ omcb/asgi.py
@@ -1,16 +1,23 @@
-"""
-ASGI config for omcb project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
-
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+
+import checks.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'omcb.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                checks.routing.websocket_urlpatterns
+            )
+        )
+    ),
+})
