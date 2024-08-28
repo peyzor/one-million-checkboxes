e05ac5e83fa2412aa8580cac3f82568f5700c0fb
Peyman <hassani.peyman627@gmail.com>
2024-08-28 13:45:25 +0330

    Increase the number of checkboxes

--- checks/views.py
+++ checks/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 
 def home(request):
-    context = {'loop_64': range(64)}
+    context = {'the_loop': range(1000)}
     return render(request, 'checks/home.html', context=context)
