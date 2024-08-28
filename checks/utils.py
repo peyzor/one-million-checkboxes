f596db7ef26d69ae22ca8957c9f651ebb57dc973
Peyman <hassani.peyman627@gmail.com>
2024-08-28 13:45:42 +0330

    Add get_initial_state function

---
+++ checks/utils.py
@@ -0,0 +1,15 @@
+from omcb import redis_connection
+
+
+def get_initial_state(redis_client):
+    checks_bitset = redis_client.get(redis_connection.CHECKS_BITSET_KEY)
+    count = redis_client.bitcount(redis_connection.CHECKS_BITSET_KEY)
+
+    statuses = []
+    for b in checks_bitset:
+        for i in range(7, -1, -1):
+            status = (b >> i) & 1
+            statuses.append(status)
+
+    context = {'statuses': enumerate(statuses), 'count': count}
+    return context
