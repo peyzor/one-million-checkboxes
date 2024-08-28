e12fa85a2dafba4544ae0256b9034dd773ca1e14
Peyman <hassani.peyman627@gmail.com>
2024-08-28 15:58:16 +0330

    Fix checks test to at least run

--- checks/tests.py
+++ checks/tests.py
@@ -1,29 +1,32 @@
 import random
-
-from django.test import TestCase
+import time
+from omcb import redis_connection
 
 from channels.testing import ChannelsLiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.webdriver import WebDriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 class ChecksSeleniumTests(ChannelsLiveServerTestCase):
+    serve_static = True
+
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.selenium = WebDriver()
-        cls.selenium.implicitly_wait(5)
+        cls.selenium = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        cls.selenium.implicitly_wait(3)
 
     @classmethod
     def tearDownClass(cls):
         cls.selenium.quit()
         super().tearDownClass()
 
-    def test_login(self):
+    def test_checks(self):
         self.selenium.get(f"{self.live_server_url}/")
-        for _ in range(10):
-            offset = random.randint(0, 1000)
-            box = self.selenium.find_element(By.ID, f'check-{offset}')
-            box.click()
-
-        print()
+        for _ in range(100):
+            offset = random.randint(0, redis_connection.CHECKS_BITSET_LENGTH - 1)
+            checkbox = self.selenium.find_element(By.ID, f'check-{offset}')
+            checkbox.click()
+            time.sleep(0.1)
