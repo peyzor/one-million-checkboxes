fa05dd7aab1f287e5de9780040f79cadb92555b1
Peyman <hassani.peyman627@gmail.com>
2024-08-28 13:44:39 +0330

    Add redis setup

--- checks/apps.py
+++ checks/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
+from omcb.redis_connection import get_redis_client, CHECKS_BITSET_KEY
 
 
 class ChecksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checks'
+
+    def ready(self):
+        redis_client = get_redis_client()
+
+        if not redis_client.exists(CHECKS_BITSET_KEY):
+            redis_client.setbit(CHECKS_BITSET_KEY, 1000 - 1, 0)
---
+++ omcb/redis_connection.py
@@ -0,0 +1,9 @@
+import redis
+
+CHECKS_BITSET_KEY = 'checks'
+
+
+def get_redis_client():
+    redis_client = redis.Redis(host='localhost', port=6379, db=0)
+
+    return redis_client
