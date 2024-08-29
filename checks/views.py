7ec5841823f1f6b3f262cf95c45671b3d9dd6de0
Peyman <hassani.peyman627@gmail.com>
2024-08-29 16:54:39 +0330

    Remove useless context

--- checks/views.py
+++ checks/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 
-from omcb import redis_connection
-
 
 def home(request):
-    context = {'the_loop': range(redis_connection.CHECKS_BITSET_LENGTH)}
-    return render(request, 'checks/home.html', context=context)
+    return render(request, 'checks/home.html')
