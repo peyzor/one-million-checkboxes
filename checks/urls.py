06f45ed8c7093707b927b7eef1768d3698f58996
Peyman <hassani.peyman627@gmail.com>
2024-08-30 14:50:27 +0330

    Add more_checks endpoint to gradually get checkboxes

--- checks/urls.py
+++ checks/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from checks import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
+    path('more-checks/', views.more_checks, name='more_checks')
 ]
--- checks/views.py
+++ checks/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 
+from checks.utils import get_checks
+from omcb import redis_connection
+
 
 def home(request):
     return render(request, 'checks/home.html')
+
+
+def more_checks(request):
+    limit = int(request.GET['limit'])
+    offset = int(request.GET['offset'])
+
+    redis_client = redis_connection.get_redis_client()
+    checks = get_checks(redis_client=redis_client, limit=limit, offset=offset)
+
+    context = {
+        'checks': checks,
+        'limit': limit,
+        'offset': limit + offset
+    }
+    return render(request, 'checks/more_checks.html', context=context)
